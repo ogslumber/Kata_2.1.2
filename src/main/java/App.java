a8f3f302ec245b5c5edb16a904a8da3757a76425
ogslumber <olivewarrior8@gmail.com>
2022-02-17 16:10:41 +0500

    Initial commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,14 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld hw1 = applicationContext.getBean("helloworld", HelloWorld.class);
+        HelloWorld hw2 = applicationContext.getBean("helloworld", HelloWorld.class);
+
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+        Cat cat2 = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println("hw1 is hw2: " + (hw1 == hw2));
+        System.out.println("cat1 is cat2: " + (cat1 == cat2));
     }
 }
